968f0c8c442ed1f2e46206685e260777c07364de
Adji Marzuki <adjimarzuki16@gmail.com>
2023-01-16 22:40:59 +0700

    Update Receptionist.java

--- src/SMD1/Receptionist.java
+++ src/SMD1/Receptionist.java
@@ -1,12 +1,15 @@
 package SMD1;
 
 //import java.io.*;
+import java.awt.Component;
 import java.util.Scanner;
+import javax.swing.*;
 
 public class Receptionist extends Users {
  private int r_id;
  private String r_name;
  Scanner sc = new Scanner(System.in);
+ private static final Component jframe = null;
 
  public int getR_id() {
   return r_id;
@@ -25,17 +28,21 @@ public class Receptionist extends Users {
  }
  
  public void Choice(){
+  Gui jframe = new Gui();
+  JOptionPane.showMessageDialog(jframe, "Hello Receptionist!");
+  String choice1;
+  choice1  = JOptionPane.showInputDialog(jframe, "Enter your choice: 1)GiveAppoint 2)GenerateBill : ");
   System.out.println();
   System.out.print("Enter your choice: 1)GiveAppoint 2)GenerateBill \t: ");
-  int choice = sc.nextInt();
+  //int choice = sc.nextInt();
         
         
-   switch(choice){        
-         case 1: GiveAppoint(); System.out.println();
+   switch(choice1){        
+         case "1": GiveAppoint(); System.out.println();
           break;
-         case 2: GenerateBill(); System.out.println();
+         case "2": GenerateBill(); System.out.println();
        break;
-         case 3:  main(null);
+         case "3":  main(null);
    }
   //sc.close();  
  }
@@ -47,8 +54,8 @@ public class Receptionist extends Users {
   String p_name =B.getP_name();
   int p_id = B.getP_id();
   int b_amt = B.getB_amt();
+  JOptionPane.showMessageDialog(jframe, "BillNO: "+b_no +"\nPatientID: "+p_id+"\nPatientName: "+p_name+"\nBillAmount: "+b_amt);
   System.out.println("BillNO: "+b_no +"\nPatientID: "+p_id+"\nPatientName: "+p_name+"\nBillAmount: "+b_amt);
-  System.out.println();
   
  }
  
@@ -60,9 +67,10 @@ public class Receptionist extends Users {
   Doctor D = new Doctor();
   int d_id = D.getD_id();
   String d_name = D.getD_name();
+  JOptionPane.showMessageDialog(jframe, "PatientID: "+p_id+"\nPatientName: "+p_name+"\nAppointment with Doctor \nDoctorID: "+d_id+
+  "\nDoctorName : "+d_name);
   System.out.println("\nPatientID: "+p_id+"\nPatientName: "+p_name+"\nAppointment with Doctor \nDoctorID: "+d_id+
     "\nDoctorName : "+d_name);
-  System.out.println();
   
  }
  
