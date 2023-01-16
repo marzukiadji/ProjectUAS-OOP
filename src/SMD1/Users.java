b9f249669eb629ec954cc990767aa562d1614129
Adji Marzuki <adjimarzuki16@gmail.com>
2023-01-16 22:40:31 +0700

    Update Users.java

--- src/SMD1/Users.java
+++ src/SMD1/Users.java
@@ -1,28 +1,48 @@
 package SMD1;
 
 //import java.io.*;
+import javax.swing.*;
 import java.util.Scanner;
 
-public class Users {
+public class Users extends JFrame {
   static Scanner sc = new Scanner(System.in);
+  static JLabel welcome ;
+  static JPanel totalGUI;
+
  public static void main(String[] args) {
-  System.out.print("Eenter your choice: 1)Receptionist 2)Patient 3)Doctor \t: ");
-  int choice = sc.nextInt();
+  //GUI
+  Gui myframeGui;
+  myframeGui = new Gui();
+  totalGUI = new JPanel();
+  welcome = new JLabel("Welcome, please start your session");
+  welcome.setLocation(0,0);
+  welcome.setSize(290,30);
+  welcome.setHorizontalAlignment(0);
+  totalGUI.add(welcome);
+
+  myframeGui.setVisible(true);
+  JOptionPane.showMessageDialog(myframeGui, "Welcome to Primaya Hospital");
+  JOptionPane.showMessageDialog(myframeGui, "Good Morning");
+
+  String choice1;
+  choice1  = JOptionPane.showInputDialog(myframeGui, "Enter your user : 1. Receptionist 2.Patient 3.Doctor?");
+
+  System.out.print("Enter your choice: 1)Receptionist 2)Patient 3)Doctor \t: ");
+  //int choice = sc.nextInt();
         
     do{
-   switch(choice){        
-         case 1: Receptionist R = new Receptionist();
+   switch(choice1){        
+         case "1": Receptionist R = new Receptionist();
            R.Choice();
           break;
-         case 2: Patient P = new Patient();
+         case "2": Patient P = new Patient();
            P.Choice();
            break;
-         case 3: Doctor D = new Doctor();
+         case "3": Doctor D = new Doctor();
            D.Choice();
            break;        
    }
-        }while(choice!=4);
-  //sc.close();
+        }while(choice1!="4");
  }
  
 }
