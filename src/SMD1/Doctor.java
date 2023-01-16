63d10f0618f785afad241b82267f34f85b96784e
Adji16 <adjimarzuki16@gmail.com>
2023-01-16 22:31:51 +0700

    add GUI

--- src/SMD1/Doctor.java
+++ src/SMD1/Doctor.java
@@ -1,12 +1,15 @@
 package SMD1;
 
+import java.awt.Component;
 import java.util.Scanner;
+import javax.swing.*;
 
 public class Doctor extends Users{
  
  private int d_id=301;
  private String d_name="Chisiya";
  Scanner sc = new Scanner(System.in);
+ private static final Component jframe = null;
  
  public int getD_id() {
   return d_id;
@@ -25,15 +28,19 @@ public class Doctor extends Users{
  }
 
  public void Choice(){
+      Gui jframe = new Gui();
+      JOptionPane.showMessageDialog(jframe, "Hello Doctor "+d_name+"!!");
+      String choice1;
+      choice1  = JOptionPane.showInputDialog(jframe, "Enter 1 for Check Patient");
   System.out.print("Enter your choice: 1)CheckPatient \t: ");
   //Scanner sc = new Scanner(System.in);
-  int choice = sc.nextInt();
+  //int choice = sc.nextInt();
         
         
-   switch(choice){        
-         case 1: CheckPatient(); System.out.println();
+   switch(choice1){        
+         case "1": CheckPatient(); System.out.println();
           break;
-         case 2: main(null); System.out.println();
+         case "2": main(null); System.out.println();
      
    }
    //sc.close();
@@ -41,8 +48,9 @@ public class Doctor extends Users{
  
  public void CheckPatient(){
   Patient P = new Patient();
-  String p_name =P.getP_name();
+  String p_name = P.getP_name();
   int p_id = P.getP_id();
+  JOptionPane.showMessageDialog(jframe, "Doctor: "+d_name+" has check patient "+p_name+" with ID : "+p_id);
   System.out.println("Doctor: "+d_name+" has check patient "+p_name+" with ID : "+p_id);
  }
 
--- src/SMD1/Patient.java
+++ src/SMD1/Patient.java
@@ -1,22 +1,29 @@
 package SMD1;
 
+import java.awt.Component;
 import java.util.Scanner;
+import javax.swing.*;
 
 public class Patient extends Users{
 
+ private static final Component jframe = null;
  private int p_id=101;
  private String p_name="Kuina Hikari";
  Scanner sc = new Scanner(System.in);
  
  public void Choice(){
+      Gui jframe = new Gui();
+      JOptionPane.showMessageDialog(jframe, "Hello Patient!!");
+      String choice1;
+      choice1  = JOptionPane.showInputDialog(jframe, "Enter 1 for paybill");
   System.out.print("Enter your choice: 1)PayBill \t: ");
   //Scanner sc = new Scanner(System.in);
-  int choice = sc.nextInt();
+  //int choice = sc.nextInt();
         
-   switch(choice){        
-         case 1: PayBill(); System.out.println();
+   switch(choice1){        
+         case "1": PayBill(); System.out.println();
             break;
-         case 2: main(null); System.out.println();
+         case "2": main(null); System.out.println();
      
    }
    //sc.close();
@@ -40,6 +47,7 @@ public class Patient extends Users{
  }
  
  public void PayBill(){
+  JOptionPane.showMessageDialog(jframe, "Thanks for Paying Your Bills!!");
   System.out.println("Thanks For Paying Bill");
  }
 
